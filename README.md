5d241e1706ce29f38181a07c7e5564d5e5bdc834
Julien Fausty <julien.fausty@mines-paristech.fr>
2020-03-16 18:31:25 +0100

    added dependencies

--- README.md
+++ README.md
@@ -18,3 +18,19 @@
 The HyperFox project is an attempt to create a general purpose finite element 
 library for running simulations in arbitrary dimensions of both parameters and 
 solutions and of arbitrary order.
+
+# Dependencies
+
+For now the project has two dependencies:
+
+- OpenMpi: For MPI parallelism support.
+
+- OpenMP: A compiler with OpenMP support should be used.
+
+- Eigen: A header library for linear algebra.
+
+- Catch2: A unittesting library
+
+In the future, the project will most likely depend on:
+
+- Petsc: For parallel linear solvers for linear algebra on spare matrices (with the requisite BLAS/LAPACK installations)
--- cmake/hyperFoxExternalDependencies.cmake
+++ cmake/hyperFoxExternalDependencies.cmake
@@ -1,3 +1,27 @@
-find_package(Catch2 REQUIRED)
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake)
+
+find_package(MPI REQUIRED)
+
+if (MPI_FOUND)
+  set (CMAKE_C_COMPILER "${MPI_C_COMPILER}")
+  set (CMAKE_CXX_COMPILER "${MPI_CXX_COMPILER}")
+  add_definitions(-DOMPI_SKIP_MPICXX)
+endif()
+
+find_package(OpenMP REQUIRED)
+
+if (OPENMP_FOUND)
+      set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+      set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+      set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
+endif()
+
+find_package(PETSc COMPONENTS CXX)
+
+include_directories(${PETSC_INCLUDES})
 
 find_package(Eigen3 REQUIRED)
+
+find_package(HDF5 REQUIRED)
+
+find_package(Catch2 REQUIRED)
