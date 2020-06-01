edcb372c69e8726562ba9fff1173fe322dee4b8b
Julien <julien.fausty@live.com>
2020-06-01 11:20:07 +0200

    new HDGTransport model

---
+++ src/model/HDGTransport.cpp
@@ -0,0 +1,70 @@
+#include "HDGTransport.h"
+
+namespace hfox{
+
+void HDGTransport::setFieldMap(const std::map<std::string, std::vector<double> > * fm){
+  std::map<std::string, std::vector<double> >::const_iterator itfm;
+  itfm = fm->find("Velocity");
+  if(itfm == fm->end()){
+    throw(ErrorHandle("HDGTransport", "setFieldMap", "one must provide a Velocity field to use the Transport model."));
+  }
+  fieldMap["Velocity"] = &(itfm->second);
+  if(timeScheme != NULL){
+    timeScheme->setFieldMap(fm);
+  }
+  HDGModel::setFieldMap(fm);
+};//setFieldMap
+
+void HDGTransport::allocate(int nDOFsPerNodeUser){
+  assembly.matrix = Add;
+  assembly.rhs = Add;
+  if(timeScheme != NULL){
+    timeScheme->allocate(nDOFsPerNodeUser);
+  }
+  HDGModel::allocate(nDOFsPerNodeUser);
+};//allocate
+
+void HDGTransport::initializeOperators(){
+  if(operatorMap.find("Convection") == operatorMap.end()){
+    operatorMap["Convection"] = new HDGConvection(refEl);
+  }
+  HDGModel::initializeOperators();
+};//initializeOperators
+
+std::vector<EVector> HDGTransport::parseVelocityVals() const{
+  const std::vector<double> * velVals = fieldMap.at("Velocity");
+  int nNodes = refEl->getNumNodes();
+  int dimVelVal = velVals->size()/nNodes;
+  int dimSpace = refEl->getDimension();
+  if(dimVelVal != dimSpace){
+    throw(ErrorHandle("HDGTransport", "parseVelocityVals", "the dimension of the velocity vector does not correspond to the dimension of the reference element"));
+  }
+  std::vector<EVector> res(nNodes, EVector::Zero(dimSpace));
+  for(int i = 0; i < nNodes; i++){
+    res[i] = EMap<const EVector>(velVals->data() + i*dimSpace, dimSpace);
+  }
+  return res;
+};//parseVelocityVals
+
+void HDGTransport::computeLocalMatrix(){
+  if(!(nodeSet and fieldSet)){
+    throw(ErrorHandle("HDGTransport", "computeLocalMatrix", "the nodes and the fields should be set before computing"));
+  }
+  computeElementJacobians();
+  ((HDGBase*)operatorMap["Base"])->setTau(*(fieldMap["Tau"]));
+  ((HDGBase*)operatorMap["Base"])->calcNormals(*elementNodes, jacobians);
+  ((Convection*)operatorMap["Convection"])->setVelocity(parseVelocityVals());
+  operatorMap["Base"]->assemble(dV, invJacobians);
+  operatorMap["Convection"]->assemble(dV, invJacobians);
+  localMatrix = *(operatorMap["Base"]->getMatrix());
+  localMatrix += *(operatorMap["Convection"]->getMatrix());
+};//computeLocalMatrix
+
+void HDGTransport::computeLocalRHS(){
+  if(!(nodeSet and fieldSet)){
+    throw("HDGTransport", "computeLocalRHS", "the nodes and the fields should be set before computing");
+  }
+  localRHS = EVector::Zero(localRHS.size());
+};//computeLocalRHS
+
+}//hfox
---
+++ src/model/HDGTransport.h
@@ -0,0 +1,50 @@
+#ifndef HDGTRANSPORT_H
+#define HDGTRANSPORT_H
+
+#include "HDGModel.h"
+#include "HDGConvection.h"
+
+namespace hfox{
+
+class HDGTransport : public HDGModel{
+
+  public:
+    /*!
+     * \brief constructor inheritance
+     */
+    using HDGModel::HDGModel;
+    /*!
+     * \brief a method to set the local fields
+     *
+     * @param pointer to a map of names and corresponding local values of fields
+     */
+    void setFieldMap(const std::map<std::string, std::vector<double> > * fm);
+    /*!
+     * \brief allocating the local matrix and rhs
+     */
+    void allocate(int nDOFsPerNode);
+  protected:
+    /*!
+     * \brief initialize all the operators for the class
+     */
+    void initializeOperators();
+    /*!
+     * \brief compute the local matrix
+     */
+    void computeLocalMatrix();
+    /*!
+     * \brief compute the local right hand side
+     */
+    void computeLocalRHS();
+    /*!
+     * \brief parse the velocity field values into vectors
+     */
+    std::vector<EVector> parseVelocityVals() const;
+
+
+};//HDGTransport
+
+}//hfox
+
+
+#endif //HDGTRANSPORT_H
